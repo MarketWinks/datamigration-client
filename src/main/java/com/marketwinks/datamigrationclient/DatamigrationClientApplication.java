3b56c4c986edb7b9912adab3ca0e304889e8a8e7
Chocks <chocku.engr@gmail.com>
2020-06-27 01:04:03 +0100

    initial commit

---
+++ src/main/java/com/marketwinks/datamigrationclient/DatamigrationClientApplication.java
@@ -0,0 +1,13 @@
+package com.marketwinks.datamigrationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamigrationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamigrationClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketwinks/datamigrationclient/DatamigrationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketwinks.datamigrationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamigrationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
